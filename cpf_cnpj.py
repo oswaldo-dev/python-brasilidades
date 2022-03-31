3bcac239db44d8f0b9ac9c4b4cc4240fd7d934ba
oswaldo-dev <oswaldo.dev.oliveira@gmail.com>
2022-03-31 14:17:53 -0300

    testei o cpf e o cnpj e esta tudo rodando de forma correta

--- cpf_cnpj.py
+++ cpf_cnpj.py
@@ -1,6 +1,7 @@
 from validate_docbr import CPF, CNPJ
 
-class documento:
+
+class Documento:
 
     @staticmethod
     def cria_documento(documento):
@@ -47,47 +48,3 @@ class DocCnpj:
         mascara = CNPJ()
         return mascara.mask(self.cnpj)
 
-# class CpfCnpj:
-#     def __init__(self, documento, tipo_documento):
-#         self.tipo_documento = tipo_documento
-#         documento = str(documento)
-#         if self.tipo_documento == "cpf":
-#             if self.cpf_eh_valido(documento):
-#                 self.cpf = documento
-#             else:
-#                 raise ValueError("CPF inválido!!")
-#         elif self.tipo_documento == "cnpj":
-#             if self.cnpj_eh_valido(documento):
-#                 self.cnpj = documento
-#             else:
-#                 raise ValueError("CNPJ inválido!!")
-#         else:
-#             raise ValueError("Documento inválido")
-#
-#     def __str__(self):
-#         if self.tipo_documento == "cpf":
-#             return self.format_cpf()
-#         elif self.tipo_documento == "cnpj":
-#             return self.format_cnpj()
-#
-#     def cpf_eh_valido(self, cpf):
-#         if len(cpf) == 11:
-#             validador = CPF()
-#             return validador.validate(cpf)
-#         else:
-#             raise ValueError("Quantidade de dígitos inválida!!")
-#
-#     def format_cpf(self):
-#         mascara = CPF()
-#         return mascara.mask(self.cpf)
-#
-#     def format_cnpj(self):
-#         mascara = CNPJ()
-#         return mascara.mask(self.cnpj)
-#
-#     def cnpj_eh_valido(self,cnpj):
-#         if len(cnpj) == 14:
-#             validate_cnpj = CNPJ()
-#             return validate_cnpj.validate(cnpj)
-#         else:
-#             raise ValueError("Quantidade de digitos inválida!!")
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from Cpf import Cpf
+from cpf_cnpj import Documento
 
-cpf = Cpf("012.345.678-90")
-print(cpf)
+# cpf = Cpf("012.345.678-90")
+# print(cpf)
+
+exemplo_cnpj = "35379838000112"
+exemplo_cpf = "48812321003"
+documento = Documento.cria_documento(exemplo_cnpj)
+documento2 = Documento.cria_documento(exemplo_cpf)
+print(documento)
+print(documento2)
